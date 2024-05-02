52873f6259eda4528933df18f25bddbe89f7efea
eemiljka <eemiljka@metropolia.fi>
2024-05-02 12:02:40 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/∼eemiljka/icons-animated/',
+  base: '/~eemiljka/icons-animated/',
 })
